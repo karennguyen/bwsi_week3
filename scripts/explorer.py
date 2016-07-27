3e96bbe18ffa21efd78d1762d594d0d60247ca1d
Karen Nguyen <karennguyen@users.noreply.github.com>
2016-07-27 14:36:20 -0400

    Create explorer.py
    brennan is still quinoa

---
+++ scripts/explorer.py
@@ -0,0 +1,78 @@
+#/usr/bin/python
+
+#uses potential field theory to have the racecar explore the world it's in
+
+#import statements
+import rospy
+import sys
+import math
+import numpy as np
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import * 
+from ackermann_msgs import AckermannDriveStamped
+
+RADIANS_PER_TICK = math.pi  / 360
+
+class Explorer():
+  
+	def __init__(self):
+		#constants for racecar speed and angle calculations
+        self.pSpeed = 0.3 #tweak
+        self.pAngle = 1
+        #positive charge behind racecar to give it a "kick" (forward vector)
+        self.propelling_charge = 10
+        #more constants
+        self.charge = 0.01
+  		self.safety_threshold = 0.3
+        
+        rospy.init_node("explorer")
+        
+        self.sub = rospy.Subscriber('scan', LaserScan, self.laser_callback)
+        self.pub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/navigation', AckermannDriveStamped)
+        rospy.on_shutdown(self.shutdown)
+        
+    '''
+    Get steering command as a tuple of (steering_angle, speed)
+    L is the laser scan data
+    '''
+	def get_driving_info(self, L):
+      
+		#variables that will be used to calculate sum of vectors 
+		force_x = 0  
+		force_y = 0 
+        
+		for i in range(len(L)):
+      		#actually finds sum of x and y components in vectors
+			force_x += self.charge /(L[i] ** 2) * math.sin(i * RADIANS_PER_TICK)
+            force_y += self.charge /(L[i] ** 2) * math.cos(i * RADIANS_PER_TICK)
+        
+        #shifts vector to go in the right direction (from behind racecar to front)
+        force_x = propelling_charge-force_x
+                                          
+        speed = self.pSpeed * math.sqrt(force_x ** 2 + force_y ** 2) * np.sign(force_x)
+        angle = self.pAngle * math.atan2(-force_y, force_x) * np.sign(force_x)
+                                                 
+  		return (speed, angle)
+                          
+	def laser_callback(self, scan):
+		cmd = get_driving_info(scan.ranges[180:901]) #returns a tuple with the speed and angle 
+		speed = cmd[0] #information given by the method
+		angle = cmd[1]
+        
+        if(min(scan.ranges[525:555]) < self.safety_threshold):
+          	print "Robot suicide RIP"
+			speed = -0.2
+            
+        msg = AckermannDriveStamped()
+        msg.drive.speed = speed
+        msg.drive.steering_angle = angle
+        self.pub.publish(msg)      
+      
+        
+	def shutdown(self):
+		self.pub.publish(AckermannDriveStamped())
+		rospy.sleep(1)
+      
+if __name__ == '__main__':
+	Explorer()
+	rospy.spin()
